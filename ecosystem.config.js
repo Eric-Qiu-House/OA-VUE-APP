1a1a036ed81d96f990e4afa91855da0241b1b206
Eric <eric.qzy@qq.com>
2025-04-22 15:26:18 +0800

    配了pm2 服务器启动方式

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,18 @@
+// 生产环境 - pm2 start ecosystem.config.js
 module.exports = {
   apps: [
     {
-      name: "oa-vue-dev",
-      script: "npm",
-      args: "run serve"
-    }
-  ]
+      name: "oa-vue-prod",
+      script: "npx",
+      args: ["serve", "-s", "/opt/OA-VUE-APP/dist", "-p", "8080"],
+      cwd: "/opt/OA-VUE-APP",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 };
